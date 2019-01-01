81398eb3b5e2838ee1507430d0bd52884b17f9fd
Qubo Qin <qubo.qin.2018@gmail.com>
2019-01-01 11:22:12 -0800

    remove chalk when print a json object

--- src/server/utils/pathtest.js
+++ src/server/utils/pathtest.js
@@ -7,8 +7,8 @@ module.exports = function () {
   console.log(chalk.yellow('__filename = %s \n__dirname = %s', __filename, path.resolve(__dirname)))
   console.log(chalk.yellow('. = %s', path.resolve('.')))
 
-  console.log(chalk.yellow(require('../../../assets/media/config.json')))
+  console.log(require('../../../assets/media/config.json'))
   console.log(chalk.yellow(fs.readFileSync(path.resolve(__dirname, '../../../assets/media/sometext.txt'), 'utf8')))
-  console.log(chalk.red.bgYellow('the next readFileSync() will throw out an exception if the working directory is not the same as the directory the script resides!'))
-  console.log(chalk.red.bgYellow(fs.readFileSync('../../../assets/media/sometext.txt', 'utf8')))
+  console.log(chalk.red.bgYellow('the next readFileSync() will throw out an exception if the working directory is not on the top of src!'))
+  console.log(chalk.red.bgYellow(fs.readFileSync('./assets/media/sometext.txt', 'utf8')))
 }
