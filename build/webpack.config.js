ba3b66b6b6f698b2fdb10bb68a62712f580043f2
Qubo Qin <qubo.qin.2018@gmail.com>
2019-01-05 13:51:28 -0800

    create a spa

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -34,7 +34,7 @@ const config = {
         use: { 
           loader: "babel-loader", 
           options: { 
-            presets: ['env'] 
+            presets: ["@babel/preset-env"]
           }
         } 
       },
@@ -63,6 +63,16 @@ const config = {
             }
           }
         ]
+      },
+      {
+        // 匹配.css文件
+        test: /\.css$/,
+        /*
+        先使用css-loader处理, 返回的结果交给style-loader处理.
+        css-loader将css内容存为js字符串, 并且会把background, @font-face等引用的图片,
+        字体文件交给指定的loader打包, 类似上面的html-loader, 用什么loader同样在loaders对象中定义, 等会下面就会看到.
+        */
+        use: ['style-loader', 'css-loader']
       },
       // file-loader(for images)
       { 
@@ -100,7 +110,19 @@ const config = {
 
     port: 2000,
     stats: 'errors-only',
-    open: true
+    open: true,
+    /*
+    historyApiFallback用来配置页面的重定向
+
+    SPA的入口是一个统一的html文件, 比如
+    http://localhost:8010/foo
+    我们要返回给它
+    http://localhost:8010/index.html
+    这个文件
+
+    配置为true, 当访问的文件不存在时, 返回根目录下的index.html文件
+    */
+    historyApiFallback: true
   },
   devtool: 'inline-source-map'
 }
